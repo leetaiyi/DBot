05e85d7262b7b1096c60fe4a7db6ce482a3fc0a0
Christopher T Lee <taiyi17@gmail.com>
2026-07-31 12:20:51 -0400

    gacha.py: fix more sign

--- gacha.py
+++ gacha.py
@@ -742,14 +742,14 @@ QUESTION_TEXT = {
     (2, 0): lambda k: (
         f"What major key has {abs(k[2])} "
         f"{'sharp' if abs(k[2]) == 1 else 'sharps'}?"
-        if k[2] < 0 else
+        if k[2] > 0 else
         f"What major key has {k[2]} "
         f"{'flat' if k[2] == 1 else 'flats'}?"
     ),
     (2, 1): lambda k: (
         f"What relative minor has {abs(k[2])} "
         f"{'sharp' if abs(k[2]) == 1 else 'sharps'}?"
-        if k[2] < 0 else
+        if k[2] >= 0 else
         f"What relative minor has {k[2]} "
         f"{'flat' if k[2] == 1 else 'flats'}?"
     )
